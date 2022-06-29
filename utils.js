a24c570611f128727a3f5324461294c375e17246
Waterlilyrr <noemict94@hotmail.it>
2022-06-29 16:35:24 +0200

    chore: styling

--- utils.js
+++ utils.js
@@ -8,12 +8,14 @@ const createCard = (parent, imgLink, title, price) => {
   const priceEl = c("p");
   const yearEl = c("p");
   const imgEl = c("img");
+  const showroomEl = c("div");
 
   wrapperEl.className = "wrapper";
   titleEl.className = "title";
   priceEl.className = "price";
   yearEl.className = "year";
   imgEl.className = "img";
+  showroomEl.className = "showroom";
 
   imgEl.setAttribute("alt", title);
   imgEl.setAttribute("src", imgLink);
@@ -22,7 +24,8 @@ const createCard = (parent, imgLink, title, price) => {
   priceEl.textContent = price;
 
   wrapperEl.append(imgEl, titleEl, priceEl);
-  parent.appendChild(wrapperEl);
+  parent.appendChild(showroomEl);
+  showroomEl.appendChild(wrapperEl);
 };
 
 export { c, createCard, q };
