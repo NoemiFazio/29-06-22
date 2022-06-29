c7ea1b0fb394358d584fc5292d429df091cf5d36
Waterlilyrr <noemict94@hotmail.it>
2022-06-29 16:50:02 +0200

    chore: fixing

--- script.js
+++ script.js
@@ -20,41 +20,6 @@ const body = {
   title: "Rick e Morty",
 };
 
-// fetch(BASE_URL)
-//   .then(res => res.json())
-//   .then(data => data.map(serie => {
-//     const imgOrPlaceholder = serie.poster || 'https://picsum.photos/200/300';
-
-//     try {
-//       // if (!serie.description) {
-//       //   throw new Error('La serie non contiene una descrizione');
-//       // }
-//       createCard(document.body, imgOrPlaceholder, serie.title, serie.year);
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }))
-
-// addSerieBtnEl.addEventListener('click', () => {
-//   fetch(BASE_URL, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(body)
-//   }).then(() => location.reload())
-// })
-
-// const id = prompt('Inserisci id: ');
-// // DELETE - http
-// // fetch(BASE_URL+'/'+id, {
-// fetch(`${BASE_URL}/${id}`, {
-//   method: 'DELETE',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-// }).then(() => {})
-
 // ------------ il nuovo GET tramite funzione async/await, il vecchio sta sopra commentato a riga 17
 GET(BASE_URL).then((data) => {
   data.map((serie) => {
@@ -89,14 +54,7 @@ addSerieBtnEl.addEventListener("click", () => {
     year: yearInputEl,
   };
   POST(BASE_URL, body).then(() => location.reload());
-
-  //   POST(BASE_URL, body).then(() => location.reload());
 });
-// ------------ il nuovo POST tramite funzione async/await, il vecchio sta sopra commentato a riga 33
-
-// addSerieBtnEl.addEventListener('click', () => {
-//   POST(BASE_URL, body).then(() => location.reload())
-// })
 
 // ------------ il nuovo DELETE tramite funzione async/await, il vecchio sta sopra commentato a riga 43
 deleteInputEl.addEventListener("input", (eventInput) => {
